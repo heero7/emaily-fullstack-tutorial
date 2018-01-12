b820514d3e59bb6c9c5fc03929b7adacd9fa7517
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-12 00:16:08 -0600

    Adding OAuth API for signing in

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 // import Express library
 const express = require('express');
+// require passport
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy; // required for Google OAuth
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'Our greeting!', id: 1234, comment: 'Changed'});
-});
+passport.use(new GoogleStrategy());
+
 
 // dynamically listen what port we need to set it to
 // if in a development environment, assign it use 5000
