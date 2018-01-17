a2aefcf717bf4f0730b2f0e7cea068cbb7b0c612
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-17 00:15:53 -0600

    Styled header component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -4,9 +4,16 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="left brand-log"><h4>Emaily</h4></a>
+                    <ul className="right">
+                        <li>
+                            <a>Login With Google</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
