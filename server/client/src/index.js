3c0a0db84b89b7cf13e0ca6f9d70b559bfcb1478
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-16 23:21:55 -0600

    adding reducers for redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
 import Emaily from "./components/App";
 
-ReactDOM.render(<Emaily />, document.getElementById("root"));
+// REDUCERS
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Emaily />
+  </Provider>,
+  document.getElementById("root")
+);
