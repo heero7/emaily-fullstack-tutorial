cf60875faa730fe676538a81e63835744f8a0ced
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-16 21:59:14 -0600

    Cleaning up some of the code, moving more of the OAuth code for Google

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,45 +1,41 @@
 // require passport
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy; // required for Google OAuth
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Google OAuth key
 const keys = require("../config/keys");
 
 // bring in users model class
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id)
-    .then(user => done(null, user))
+  User.findById(id).then(user => done(null, user));
 });
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: "/auth/google/callback", // this is the url to send user, after permission has been granted
-        proxy: true // allows calculating URL correctly
-      },
-      (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id})
-        .then(existingUser => {
-            if (existingUser) {
-                // if existingUser exisits
-                // we already have a record with the given profile ID
-                done(null, existingUser); // pass in null, no errors occured. Then pass the currentUser
-            } else {
-                // we don't have arecord with this ID
-                // create a new record
-                new User({googleId: profile.id}).save()
-                .then(user => done(null, user));
-            }
-        });
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback", // this is the url to send user, after permission has been granted
+      proxy: true // allows calculating URL correctly
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        // if existingUser exisits
+        // we already have a record with the given profile ID
+        return done(null, existingUser); // pass in null, no errors occured. Then pass the currentUser
       }
-    )
-  );
+        // we don't have arecord with this ID
+        // create a new record
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+    }
+  )
+);
