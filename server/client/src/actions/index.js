083343e7b93bd9a5b722e2bb15482f0b5eef42dd
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-17 22:33:24 -0600

    Created first fetch user action creator

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,7 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { FETCH_USER } from './types';
+import { FETCH_USER } from "./types";
 
 const fetchUser = () => {
-    axios.get('/api/current_user');
+  return function(dispatch) {
+    axios
+      .get("/api/current_user")
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
