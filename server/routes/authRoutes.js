5c9b6ce310aa8065453c9b688ebd991d5bcb43c4
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-18 00:06:31 -0600

    Logout and login buttons have full functionality

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,35 +1,34 @@
 // Route Handlers for Authorization
-const passport = require('passport');
+const passport = require("passport");
 
+module.exports = app => {
+  app.get("/", (req, res) => {
+    res.send("Home! The first page!");
+  });
 
-module.exports = (app) => {
-    app.get("/", (req, res) => {
-        res.send("Home! The first page!");
-      });
-      
-      app.get(
-        "/auth/google",
-        passport.authenticate("google", {
-          scope: ["profile", "email"]
-        })
-      );
-      
-      app.get(
-        "/auth/google/callback", 
-        passport.authenticate("google"),
-        (req, res) => {
-          res.redirect('/surveys');
-        }
-      );
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
+    })
+  );
 
-      // when the user wants to logout route
-      app.get('/api/logout', (req, res) => {
-        req.logout(); // delete cookie that is attached to login
-        res.send(req.user);
-      });
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
-      // get the current user route
-      app.get('/api/current_user', (req, res) => {
-        res.send(req.user);
-      });
+  // when the user wants to logout route
+  app.get("/api/logout", (req, res) => {
+    req.logout(); // delete cookie that is attached to login
+    res.redirect("/"); // redirect to root route
+  });
+
+  // get the current user route
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
