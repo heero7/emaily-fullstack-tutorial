3fdb054d9acd7050a5d5ed8ae878b3ccb187044f
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-17 23:22:26 -0600

    Refactored index-actions, can now get current user that is logged in

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 import { FETCH_USER } from "./types";
 
-const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+/**
+ * Get the currently logged in user
+ * Call to the /api/current_user
+ * this gets the data tied to the user object
+ */
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res });
 };
