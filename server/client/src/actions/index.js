ba955bb0aefa28ea9aba893f0cf76c396f3d62dd
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-17 23:50:03 -0600

    App shows new text if logged in, logged out, or pending connect

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -6,8 +6,10 @@ import { FETCH_USER } from "./types";
  * Get the currently logged in user
  * Call to the /api/current_user
  * this gets the data tied to the user object
+ * 
+ * We give the reducer, res.data - the user object
  */
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
-    render() {
+  /**
+   * Renders necessary content for user login
+   * If the user is not logged in, show log in button
+   * If the user is pending login.. show nothing
+   * If the user is logged in show something else
+   */
+  renderLoginContent() {
+    switch (this.props.auth) {
+      case null:
+        return "S";
+      case false:
         return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a className="left brand-logo">Emaily</a>
-                    <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
         );
+      default:
+        return <li><a>Logout</a></li>;
     }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">{this.renderLoginContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header;
+// Get state from redux store
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+// Setup connect
+export default connect(mapStateToProps)(Header);
