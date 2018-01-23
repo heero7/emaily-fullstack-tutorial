a079411fb8e1cdd0cb4f6ff5820f25df7cd978f9
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-22 23:01:40 -0600

    Refactored Survey... created recipient schema as subdocument for mongoose

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+const RecipientSchema = require("./Recipient");
 
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recipients: [String], // array containing list of strings
+    recipients: [RecipientSchema], // array containing list of strings
+    yes: { type: Number, default: 0},
+    no: { type: Number, default: 0 }
 });
 
 mongoose.model("surveys", surveySchema);
