afc0a15fd7632d3393fd85949d5da5e287fb9fe9
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-22 23:07:01 -0600

    More Surveys refactoring

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -9,7 +9,11 @@ const surveySchema = new Schema({
     subject: String,
     recipients: [RecipientSchema], // array containing list of strings
     yes: { type: Number, default: 0},
-    no: { type: Number, default: 0 }
+    no: { type: Number, default: 0 },
+    // every Survey will link to a user. We are linking the id for this record
+    _user: { type: Schema.Types.ObjectId, ref: 'User'},
+    dateSent: Date,
+    lastResponded: Date
 });
 
 mongoose.model("surveys", surveySchema);
