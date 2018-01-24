8a75b08b01cd3ca35901ffcf0e9bdaa19b827dd1
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-24 00:08:47 -0600

    Started styling Dashboard, brought in materialize icons

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 // COMPONENTS
-import Header from './Header';
-import Landing from './Landing';
+import Header from "./Header";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
 // dummy components
-const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 
 class App extends Component {
-  
   componentDidMount() {
     this.props.fetchUser();
   }
