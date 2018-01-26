47f33db1695b87fa5504bd3a05eec2cc134a1c09
Kevin Mudiandambo <kevin7.mudia@gmail.com>
2018-01-25 19:47:18 -0600

    Added validation for survey form

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Bring in the StripeCheckout
-import StripeCheckout from 'react-stripe-checkout';
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 /**
  * Stripe Checkout wrapper
  */
 class Payments extends Component {
-    // constructor(props) {
-    //     super(props);
-    // }
+  // constructor(props) {
+  //     super(props);
+  // }
 
-
-
-    render() {
-        return (
-            <StripeCheckout
-                name="Emaily"
-                description="$5 for 5 credits!"
-                amount={500}
-                token={token => this.props.handleToken(token)}
-                stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            >
-                <button className="btn" >Add Credits</button>
-            </StripeCheckout>
-        );
-    }
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 credits!"
+        amount={500}
+        token={token => this.props.handleToken(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
 }
 
-export default connect(null,actions)(Payments);
+export default connect(null, actions)(Payments);
--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -8,6 +8,8 @@ import { reduxForm, Field } from "redux-form"; // identical to connect in redux
 // COMPONENTS
 import SurveyField from "./SurveyField";
 
+import validateEmails from "../../utils/validateEmails";
+
 const FIELDS = [
   { label: "Survey Title", name: "title" },
   { label: "Subject Line", name: "subject" },
@@ -47,6 +49,25 @@ class SurveyForm extends Component {
   }
 }
 
+function validate(values) {
+    const errors = {};
+
+    errors.email = validateEmails(values.email || "");
+
+    _.each(FIELDS, ({ name }) => {
+        if (!values[name]) {
+            errors[name] = "You must provide a value";
+        }
+    });
+
+    
+
+    return errors;
+}
+
 // only passes one parameter, for options
 // Note: handleSubmit is given to us via reduxForm
-export default reduxForm({ form: "surveyForm" })(SurveyForm);
+export default reduxForm({
+  validate,
+  form: "surveyForm"
+})(SurveyForm);
